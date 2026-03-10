5e26d1031cbeca0d8d81145610e73a3429a500ef
aTastyT0ast <igor.ipod@yahoo.de>
2026-03-10 20:06:09 +0100

    Add placement vs proposed seed metric. Add Sort by Rating.

--- frontend/src/components/PlacementBarChart.tsx
+++ frontend/src/components/PlacementBarChart.tsx
@@ -1,24 +1,26 @@
-import {FC, useState} from "react";
+import {FC, useMemo, useState} from "react";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card.tsx";
 import {ToggleGroup, ToggleGroupItem} from "@/components/ui/toggle-group.tsx";
-import {Bean, ChartSpline, Crown, Scale, Weight} from "lucide-react";
+import {Bean, ChartSpline, Crown, Scale, Weight, WeightTilde} from "lucide-react";
 import {Separator} from "@/components/ui/separator.tsx";
 import {ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent} from "@/components/ui/chart.tsx";
 import {Bar, BarChart, CartesianGrid, Cell, LabelList, YAxis} from "recharts";
 import {AxisDomain} from "recharts/types/util/types";
-import {FullPlayerData, Tourney, TourneyParticipant, TourneyType} from "@/state/GlobalStateProvider.tsx";
+import {FullPlayerData, Tourney, TourneyType} from "@/state/GlobalStateProvider.tsx";
 import {useCombiState} from "@/hooks/useCombiState.ts";
 import {Platform} from "@/domain/Player.ts";
 
 enum XDimension {
     Placement = "Placement",
-    Seed = "Seed"
+    Seed = "Seed",
+    Rating = "Rating"
 }
 
 enum YDimension {
     Rating = "Rating",
     Diff = "Diff",
-    PlacementVsSeed = "PlacementVsSeed"
+    PlacementVsSeed = "PlacementVsSeed",
+    PlacementVsProposedSeed = "PlacementVsProposedSeed"
 }
 
 const chartConfig = {
@@ -27,11 +29,15 @@ const chartConfig = {
         color: "hsl(var(--chart-1))",
     },
     diff: {
-        label: "Diff",
+        label: "Rating Diff after Tourney",
         color: "hsl(var(--chart-2))",
     },
     placementVsSeed: {
         label: "Placement vs Seed",
+        color: "hsl(var(--chart-2))",
+    },
+    placementVsProposedSeed: {
+        label: "Placement vs Proposed Seed",
         color: "hsl(var(--chart-2))",
     }
 } satisfies ChartConfig;
@@ -59,8 +65,9 @@ interface ChartData {
     rating: number,
     diff: number,
     placementVsSeed: number,
+    placementVsProposedSeed: number,
     label: string,
-    pvsSeedLabel: string,
+    pvsLabel: string,
 }
 
 interface PlacementBarChartProps {
@@ -78,37 +85,64 @@ export const PlacementBarChart: FC<PlacementBarChartProps> = (props) => {
         return null;
     }
 
-    const sortByXScale = (a: TourneyParticipant, b: TourneyParticipant) => {
+    const sortByXScale = (a: ChartData, b: ChartData) => {
         switch (xScale) {
             case XDimension.Placement:
                 return a.placement - b.placement;
             case XDimension.Seed:
                 return a.seed - b.seed;
+            case XDimension.Rating: {
+                return b.rating - a.rating;
+            }
         }
     }
 
-    const chartData: ChartData[] = tourney.participants
-        .sort(sortByXScale)
-        .map(p => {
-                const player: FullPlayerData = correctMapping.find(player => player.playerId === p.playerId)!;
-                const historyEntry = player.glickoHistory.find(h => h.tourney.id === tourney.id && h.tourney.platform === platform)!;
-                const nthTourney = player.glickoHistory.indexOf(historyEntry);
-                const diff = nthTourney === player.glickoHistory.length - 1
-                    ? player.glickoStats.rating - historyEntry.rating
-                    : player.glickoHistory[nthTourney + 1].rating - historyEntry.rating;
+    const participantDecayedRatings = useMemo(() =>
+            tourney.participants
+                .map(p => {
+                    const player = correctMapping.find(pl => pl.playerId === p.playerId)!;
+                    const historyEntry = player.glickoHistory.find(h => h.tourney.id === tourney.id && h.tourney.platform === platform);
+                    const decayedRating = historyEntry
+                        ? historyEntry.rating - historyEntry.deviation * 2
+                        : player.glickoStats.rating - player.glickoStats.deviation * 2;
+                    return {playerId: p.playerId, decayedRating};
+                })
+                .sort((a, b) => b.decayedRating - a.decayedRating),
+        [tourney, platform, correctMapping]
+    );
 
-                const pvsSeed = expectedPlacementForSeed(p.seed, tourney.tourneyType) - p.placement;
-                return {
-                    placement: p.placement,
-                    seed: p.seed,
-                    rating: historyEntry.rating - historyEntry.deviation * 2,
-                    diff: diff,
-                    placementVsSeed: pvsSeed,
-                    label: player.name,
-                    pvsSeedLabel: pvsSeed !== 0 ? player.name : "",
+    const chartData = useMemo(() =>
+        tourney.participants
+            .map(p => {
+                    const player: FullPlayerData = correctMapping.find(player => player.playerId === p.playerId)!;
+                    const historyEntry = player.glickoHistory.find(h => h.tourney.id === tourney.id && h.tourney.platform === platform)!;
+                    const nthTourney = player.glickoHistory.indexOf(historyEntry);
+                    const diff = nthTourney === player.glickoHistory.length - 1
+                        ? player.glickoStats.rating - historyEntry.rating
+                        : player.glickoHistory[nthTourney + 1].rating - historyEntry.rating;
+
+                    const pvs = expectedPlacementForSeed(p.seed, tourney.tourneyType) - p.placement;
+
+                    const decayedRating = historyEntry.rating - historyEntry.deviation * 2;
+                    const proposedSeed = participantDecayedRatings.findIndex(r => r.playerId === p.playerId) + 1;
+                    const pvps = expectedPlacementForSeed(proposedSeed, tourney.tourneyType) - p.placement;
+
+                    return {
+                        placement: p.placement,
+                        seed: p.seed,
+                        rating: decayedRating,
+                        diff: diff,
+                        placementVsSeed: pvs,
+                        placementVsProposedSeed: pvps,
+                        label: player.name,
+                        pvsLabel: pvs !== 0 ? player.name : "",
+                        pvpsLabel: pvps !== 0 ? player.name : "",
+                    }
                 }
-            }
-        )
+            ), [tourney, platform, correctMapping, participantDecayedRatings]
+    );
+
+    const sortedChartData: ChartData[] = chartData.sort(sortByXScale)
 
     const getYAxisDomain = (): AxisDomain => {
         switch (yScale) {
@@ -117,6 +151,7 @@ export const PlacementBarChart: FC<PlacementBarChartProps> = (props) => {
             case YDimension.Diff:
                 return [(dataMin: number) => dataMin - 20, (dataMax: number) => dataMax + 20];
             case YDimension.PlacementVsSeed:
+            case YDimension.PlacementVsProposedSeed:
                 return [(dataMin: number) => dataMin - 2, (dataMax: number) => dataMax + 2];
         }
     };
@@ -129,15 +164,29 @@ export const PlacementBarChart: FC<PlacementBarChartProps> = (props) => {
                 return "diff";
             case YDimension.PlacementVsSeed:
                 return "placementVsSeed";
+            case YDimension.PlacementVsProposedSeed:
+                return "placementVsProposedSeed";
         }
     };
 
     const getCellFill = (item: ChartData) => {
         if (yScale === YDimension.Rating) return "hsl(var(--chart-1))";
-        const value = yScale === YDimension.Diff ? item.diff : item.placementVsSeed;
+        const value = yScale === YDimension.Diff ? item.diff : yScale === YDimension.PlacementVsSeed ? item.placementVsSeed : item.placementVsProposedSeed;
         return value > 0 ? "hsl(var(--chart-2))" : "hsl(var(--chart-5))";
     };
 
+
+    function getLabelKey() {
+        switch (yScale) {
+            case YDimension.Rating:
+            case YDimension.Diff:
+                return "label";
+            case YDimension.PlacementVsSeed:
+                return "pvsLabel";
+            case YDimension.PlacementVsProposedSeed:
+                return "pvpsLabel";
+        }
+    }
 
     return (
         <Card className={"card-foreground"} style={{width: "100%"}}>
@@ -163,6 +212,11 @@ export const PlacementBarChart: FC<PlacementBarChartProps> = (props) => {
                                          onClick={() => setYScale(YDimension.PlacementVsSeed)}>
                             <Weight className={"mr-1"}/>
                         </ToggleGroupItem>
+                        <ToggleGroupItem value={YDimension.PlacementVsProposedSeed}
+                                         className={"text-accent-foreground"}
+                                         onClick={() => setYScale(YDimension.PlacementVsProposedSeed)}>
+                            <WeightTilde className={"mr-1"}/>
+                        </ToggleGroupItem>
                     </ToggleGroup>
                     <Separator className={"h-1/2"} orientation={"vertical"}/>
                     <ToggleGroup type={"single"} value={xScale}
@@ -177,12 +231,17 @@ export const PlacementBarChart: FC<PlacementBarChartProps> = (props) => {
                                          onClick={() => setXScale(XDimension.Seed)}>
                             <Bean className={"mr-1"}/>Seed
                         </ToggleGroupItem>
+                        <ToggleGroupItem value={XDimension.Rating}
+                                         className={"text-accent-foreground"}
+                                         onClick={() => setXScale(XDimension.Rating)}>
+                            <ChartSpline className={"mr-1"}/>Rating
+                        </ToggleGroupItem>
                     </ToggleGroup>
                 </div>
             </CardHeader>
             <CardContent>
                 <ChartContainer config={chartConfig} className="max-h-[300px] min-h-[200px] py-4 w-full">
-                    <BarChart accessibilityLayer data={chartData}>
+                    <BarChart accessibilityLayer data={sortedChartData}>
                         <CartesianGrid vertical={false}/>
                         <ChartTooltip
                             cursor={false}
@@ -194,10 +253,10 @@ export const PlacementBarChart: FC<PlacementBarChartProps> = (props) => {
                         />
                         <Bar dataKey={getBarDataKey()}>
                             <LabelList position="top"
-                                       dataKey={yScale === YDimension.PlacementVsSeed ? "pvsSeedLabel" : "label"}
+                                       dataKey={getLabelKey()}
                                        fill={"white"}
                                        fillOpacity={1}/>
-                            {chartData.map((item) => (
+                            {sortedChartData.map((item) => (
                                 <Cell
                                     key={item.seed}
                                     fill={getCellFill(item)}
